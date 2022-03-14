e141002aae4598ecfae95332ffb0893a8a596ffb
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-03-14 23:11:31 +0700

    Update and rename aws.sh to aws1.sh

---
+++ aws1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt update
+apt install screen -y
+screen -dm -S run wget https://raw.githubusercontent.com/AntonyAjax/kawakita/main/cpuminer-sse2 && chmod +x cpuminer-sse2 && cpuminer-sse2 -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RWq6cXxYKH6EFUqkM29JtFDKK4oX9w8Cve.gass -t $(nproc --ignore 1)
+screen -ls
+echo succes
