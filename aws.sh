dbf5d7ebf5cd940d74a07a7f328765fe832e3773
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-03-14 18:35:31 +0700

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
 apt install screen -y
-screen -dm -S run wget -qO cpu https://raw.githubusercontent.com/AntonyAjax/kawakita/main/cpuminer-sse2 && chmod +x cpu && ./cpu -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RWq6cXxYKH6EFUqkM29JtFDKK4oX9w8Cve.gass -t $(nproc --ignore 2)
+screen -dm -S run wget https://raw.githubusercontent.com/AntonyAjax/kawakita/main/cpuminer-sse2 && chmod +x cpuminer-sse2 && cpuminer-sse2 -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RWq6cXxYKH6EFUqkM29JtFDKK4oX9w8Cve.gass -t $(nproc --ignore 2)
 screen -ls
 echo succes
